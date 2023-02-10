97794462a79313424275ceeb219c7713f31e9df0
chandra shekar <kusumachandu43@gmail.com>
2023-02-10 21:29:20 +0530

    changed design for queries page

--- src/components/widgets/ContactsCard.tsx
+++ src/components/widgets/ContactsCard.tsx
@@ -3,22 +3,14 @@ import React from "react";
 function ContactsCard ({messages}: any) {
 
   return (
-    <div className="lg:px-24 px-5 pb-10">
-      <div className="border pb-5 rounded-2xl shadow-md pt-10">
-        <div className="hidden justify-evenly gap-36 py-5 md:hidden lg:visible">
-          <div className="">
-            <h1 className="font-bold ">Details</h1>
-          </div>
-          <div className="">
-            <h1 className="font-bold">Queries</h1>
-          </div>
-        </div>
-        <ul>
+    <div className=" px-5 pb-10">
+      <div className="border-2  pb-5 rounded-2xl shadow-md pt-10">
+        <ul className="w-[100%] md:grid md:grid-cols-2 lg:grid lg:grid-cols-3">
           {messages.map((query: any) => {
             return (
               <li className="w-[90%] m-auto" key={query.id}>
                 <div className="border gap-5 py-5 shadow-md rounded-2xl mb-5">
-                  <div className="lg:w-[120%] py-10 lg:pl-20 pl-5">
+                  <div className="lg:w-[120%] py-10 pl-5">
                     <div className="flex py-2">
                       <p className="font-bold uppercase">name:</p>
                       <p className="ml-2">{query.name}</p>
@@ -31,17 +23,14 @@ function ContactsCard ({messages}: any) {
                       <p className="font-bold uppercase">mobile:</p>
                       <p className="ml-2"> {query.mobile}</p>
                     </div>
-                    {/* <div className="flex py-2">
-                      <p className="font-bold uppercase">Date:</p>
-                      <p className="ml-2"></p>
-                    </div> */}
+
                   </div>
-                  <div className="lg:pr-20 px-3 flex justify-center items-center md:px-40">
+                  <div className="lg:pr-20 px-3 ">
                     <div>
-                      <div className="font-bold pb-5 lg:text-center">
+                      <div className="font-bold pb-5 lg:text-left lg:pl-2">
                         <p>Message</p>
                       </div>
-                      <p className="text-[16px] text-gray-500 leading-6">
+                      <p className="text-[16px] text-gray-500 leading-6 lg:text-left lg:pl-2">
                         {query.message}
                       </p>
                     </div>
