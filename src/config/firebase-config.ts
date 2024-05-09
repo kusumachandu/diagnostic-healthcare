50d6938f4149c82af4deed72206c122b0f512aa2
kusumachandu <kusumachandu43@gmail.com>
2024-05-10 02:36:43 +0530

    fix(build errors): resovled build errors

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,6 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from '@firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -9,10 +9,10 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
-const app = initializeApp(firebaseConfig, 'healthcare');
+const app = initializeApp(firebaseConfig, "Healthcarewebsite");
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
